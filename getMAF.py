30024e5b0270c69c9ee8cc4221236be4975f6a7c
Elysire <sidwell@rigade.com>
2016-12-09 18:06:30 +0100

    get maf only from annote files 32 and 34

--- getMAF.py
+++ getMAF.py
@@ -5,16 +5,16 @@
 Created on Thu Nov 03 09:28:09 2016
 @author: Sidwell Rigade
 Description:
-	Get MAF from anotation file or from an adding file with missing MAF from dbSNP
+	Get MAF from anotation file32 or 34
 
 Usage:
-    getMAF.py --listSNP=file --annotFile=File --missingMAF=File
+    getMAF.py --snpList=file --annotFile1=File --annotFile2=File
 
 Options:
     -h --help       Show help.
-    listSNP         list of SNP to genotype
-    annotFile       annotation file
-    missingMAF      MAF that are not in annote file
+    snpList         list of SNP to genotype
+    annotFile1      annotation file32
+    annotFile2      Anntotation file34
 
 """
 
@@ -35,50 +35,53 @@ def import_list_snp(listSNP):
             list_snp.append(snp)
     return list_snp
 
-def getMAF_from_annot_file(filepath,name_snp): # get MAF for each snp 
+def getMAF_from_annot_file34(filepath,name_snp): # get MAF for each snp 
     df = pd.read_csv(filepath,sep=',',header=19,low_memory=False)
-    if df[df['dbSNP RS ID']==name_snp]['Minor Allele Frequency'].any():
-        line = df[df['dbSNP RS ID']==name_snp]['Minor Allele Frequency']
-        line = line.str.split("/")
-        allele_frequency=line.iloc[0][0]
-        return allele_frequency
-    else:
-        allele_frequency="snp not in anotation file"
-    return allele_frequency
+    if df[df['dbSNP RS ID']==name_snp]['Allele Frequencies'].any():
+        lineFreq = df[df['dbSNP RS ID']==name_snp]['Allele Frequencies']
+        if lineFreq.item() != "---":
+            lineFreq = lineFreq.str.split("//")
+            freq_A = lineFreq.item()[0]
+            freq_B = lineFreq.item()[1]
+            if freq_B < freq_A :
+                maf = [freq_B,"B"]
+            if freq_A < freq_B:
+                maf = [freq_A,"A"]
+            return maf
+        else:
+            maf=lineFreq.item()
+    else :
+        maf = "---"
+    return maf
 
-def getMAFfromMAF_file(MAFfile):
-    with open(MAFfile,'r') as f:
-        list_maf_snp=[]
-        for line in f.readlines():
-            couple_snp_maf=[]
-            line = line.replace("\n","")
-            line = line.split("\t")
-            name_snp = line[0]
-            maf = line[1]
-            couple_snp_maf.append(name_snp)
-            couple_snp_maf.append(maf)
-            list_maf_snp.append(couple_snp_maf)
-        return list_maf_snp
+def getMAF_from_annot_file32(filepath,name_snp):
+    df = pd.read_csv(filepath,sep=',',header=20,low_memory=False)
+    if df[df['dbSNP RS ID']==name_snp]['Allele Frequencies'].any():
+        lineFreq = df[df['dbSNP RS ID']==name_snp]['Allele Frequencies']
+        lineFreq = lineFreq.str.split("//")
+        freq_A = lineFreq.item()[3]
+        freq_A = freq_A.replace("/ ","")
+        freq_B = lineFreq.item()[4]
+        if freq_B < freq_A :
+            maf = [freq_B,"B"]
+        if freq_A < freq_B:
+            maf = [freq_A,"A"]
+    return maf
+
     
 def main():
     args = docopt.docopt(__doc__)
     tablo_all_maf=[]
-    listSNP = import_list_snp(args['--listSNP'])
-    missing_maf_file = getMAFfromMAF_file(args['--missingMAF'])
+    listSNP = import_list_snp(args['--snpList'])
+    #listSNP=['rs34440822']
     for snp in listSNP:
-        if snp in chain(*missing_maf_file):
-            for i in missing_maf_file:
-                if i[0] == snp:
-                    maf = i[1]
-        if snp not in chain(*missing_maf_file):
-            maf = getMAF_from_annot_file(args['--annotFile'],snp)
-        if maf == "snp not in anotation file" or maf == "---":
-            print(snp,"can't get maf")
-            continue
-        couple_snp_maf_final = [snp,maf]
+        maf = getMAF_from_annot_file34(args['--annotFile2'],snp)
+        if maf == "---":
+            maf = getMAF_from_annot_file32(args['--annotFile1'],snp)
+        couple_snp_maf_final = [snp,maf[0],maf[1]]
         print(couple_snp_maf_final)
         tablo_all_maf.append(couple_snp_maf_final)
-    all_maf = pd.DataFrame(tablo_all_maf,columns=['snp','maf'])
+    all_maf = pd.DataFrame(tablo_all_maf,columns=['snp','maf','allele'])
     print(all_maf)
     all_maf.to_csv(path_or_buf="all_maf_4_GM.txt")
     return all_maf
