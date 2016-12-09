b58f9d07d20252ebd1165393da6ccf3f6ffc9694
Elysire <sidwell@rigade.com>
2016-12-09 18:06:07 +0100

    updated priors and add maf obs

--- GaussianMixture_priors_all_init.py
+++ GaussianMixture_priors_all_init.py
@@ -9,14 +9,17 @@ Description:
 	snp in 3 genotypes.
 
 Usage:
-    GaussianMixture.py --snpList=File --callingRAC=File
+    GaussianMixture.py --snpList=File --callingRAC=File --correction=<BOOL>
 
 Options:
-    -h --help     Show help.
-    snpList		list snp txt
-    callingRAC		results snp
+    -h --help			Show help
+    -V --version		Show the version and exit
+    --snpList			list snp txt
+    --callingRAC		results snp
+    --correction=BOOL	Perform correction ? True or False [default: True]  
 
 """
+__version__ = '0.0.1'
 
 try:
     import docopt
@@ -35,6 +38,7 @@ import itertools
 import progressbar
 from time import sleep
 from hmmlearn import hmm
+import math
 pd.set_option('chained',None) #pour eviter le warning sur les copies de dataframe
 
 
@@ -59,7 +63,9 @@ def import_list_snp(listSNP):
 def getMAF(fileAllMAF,name_snp):
 	dfMAF = pd.read_csv(fileAllMAF)
 	MAFsnp = dfMAF.loc[dfMAF.snp==name_snp]['maf'].item()
-	return float(MAFsnp)
+	alleleMAF = dfMAF.loc[dfMAF.snp==name_snp]['allele'].item()
+	MAF_allele = [MAFsnp,alleleMAF]
+	return MAF_allele
 
 ############## HOMOGENEIZATION PLATES (CORRECTION) #############
 def quartile_subset(logratios,lower,upper): # avoid outliers 
@@ -96,6 +102,12 @@ def correct_strength_median(df): # correct strength by median
 
 ############ PARAMETERS TO DO GAUSSIAN MIXTURE #############
 def getParamsFromDatas(df_corrected, genotypes): # Get weigths for each genotype and means for each genotype for LogRatio and Strength -> choose datas corrected per plate or not 
+	if args['--correction'] == 'True':
+		LogRatio = "CorrectedLogRatio"
+		Strength = "CorrectedStrength"
+	else:
+		LogRatio = "Log Ratio"
+		Strength = "Strength"
 	df = df_corrected
 	df=df.rename(columns = {'Forced Call':'Forced_Call'}) #change name to be used in df.Forced_Call because spaces bugs
 	wgts=[]
@@ -107,17 +119,12 @@ def getParamsFromDatas(df_corrected, genotypes): # Get weigths for each genotype
 		countGeno.append(nbGeno)
 		wgt = nbGeno/len(df) #count genotype i (AA or AB or BB) normalize with len
 		wgts.append(wgt)
-		#### !!!!!!!!!!!  choose "Log Ratio" or "CorrectedLogRatio"
-		#meanLog = (df[df.Forced_Call==i].mean()["Log Ratio"])
-		meanLog = (df[df.Forced_Call==i].mean()["CorrectedLogRatio"]) #mean of logRatio for genotype i
+		meanLog = (df[df.Forced_Call==i].mean()[LogRatio]) #mean of logRatio for genotype i
 		meansCouple.append(meanLog)
-		#### !!!!!!!!!!!  choose "Strength" or "CorrectedStrength"
-		#meanStrength = (df[df.Forced_Call==i].mean()["Strength"])
-		meanStrength = (df[df.Forced_Call==i].mean()["CorrectedStrength"]) #mean of Strength for genotype i
+		meanStrength = (df[df.Forced_Call==i].mean()[Strength]) #mean of Strength for genotype i
 		meansCouple.append(meanStrength)
 		means.append(meansCouple)
-		#pvar=np.var(df["Strength"])
-		pvar=np.var(df["CorrectedStrength"])
+		pvar=np.var(df[Strength])
 	wgts = np.asarray(wgts)
 	means_array = np.asarray(means)
 	return wgts, means_array, countGeno, pvar
@@ -165,6 +172,14 @@ def effectifs(results):
 			nbBB+=1
 	effectifsAfterGM = [nbAA,nbAB,nbBB]
 	return effectifsAfterGM
+def getMAFobs(effectifs_predicted):
+	freq_A = ((2 * effectifs_predicted[0]) + effectifs_predicted[1]) / (2 * (sum(effectifs_predicted)))
+	freq_B = ((2 * effectifs_predicted[2]) + effectifs_predicted[1]) / (2 * (sum(effectifs_predicted)))
+	if freq_A < freq_B:
+		mafOBS = [freq_A,'A']
+	else :
+		mafOBS = [freq_B,'B']
+	return mafOBS
 
 #################### OUTPUTS ######################
 def writeProbasConfidence(df,FileOut,probasGM): # write a file with probabilities and confidence score for each genotype 
@@ -190,7 +205,7 @@ def writeMeans(FileOut,means,genotypes): # write a file with means for each geno
 		for j in mean:
 			fichier.write(str(j)+"\t")
 		fichier.write("\n")
-def plot_results(datas,resultGM,means,covariances,name_snp,index_ADN_103,score,maf,HB): # plot results on graph 
+def plot_results(datas,resultGM,means,covariances,name_snp,index_ADN_103,score,maf,HB,MAF_obs): # plot results on graph 
     colors_genotypes = ['pink' if i==0 else 'skyblue' if i==1 else 'lightgreen' for i in resultGM]
     colors_ellipses = ['navy', 'cyan', 'cornflowerblue']
     plt.clf()
@@ -214,23 +229,32 @@ def plot_results(datas,resultGM,means,covariances,name_snp,index_ADN_103,score,m
     plt.title("Calling "+name_snp)
     plt.xlabel('LogRatio')
     plt.ylabel('Strength')
-    plt.text(0.015, 0.95,"Score homogeneite = "+str(score), ha='left', va='center', transform=ax.transAxes)
-    plt.text(0.015, 0.90,"MAF = "+str(maf), ha='left', va='center', transform=ax.transAxes)
-    plt.text(0.015, 0.85,"HB = "+str(HB), ha='left', va='center', transform=ax.transAxes)
+    plt.text(0.015, 0.98,"Score homogeneite = "+str(score),fontsize=8, ha='left', va='center', transform=ax.transAxes)
+    plt.text(0.015, 0.95,"MAF_theo = "+str(maf[0])+"  allele:"+str(maf[1]),fontsize=8, ha='left', va='center', transform=ax.transAxes)
+    plt.text(0.015, 0.92,"HB = "+str(HB),fontsize=8, ha='left', va='center', transform=ax.transAxes)
+    plt.text(0.015, 0.89,"MAF_obs = "+str(MAF_obs[0])+"  allele:"+str(MAF_obs[1]),fontsize=8, ha='left', va='center', transform=ax.transAxes)
     #plt.show()
-    plt.savefig(str(name_snp)+"_gm_corrected.png")
+    plt.savefig(str(name_snp)+"_gm_priors_corrected.png")
 
 ############### GAUSSIAN MIXTURE #############
 def GaussianMixture(df,wgts,means,genotypes,name_snp,index_ADN_103,maf,priors,pvar): # do the GM with EM algorithm and output results 
-	#datas = df[['Log Ratio','Strength']].as_matrix()
-	datas = df[['CorrectedLogRatio','CorrectedStrength']].as_matrix()
+	if args['--correction'] == 'True':
+		LogRatio = "CorrectedLogRatio"
+		Strength = "CorrectedStrength"
+		means_initialisation = priors
+	else:
+		LogRatio = "Log Ratio"
+		Strength = "Strength"
+		means_initialisation = means
+
+	datas = df[[LogRatio, Strength]].as_matrix()
 	precisions = np.diag([pvar]*2)
-	maf = float(maf)
-	freqBB = maf**2
-	freqAB = 2 * maf * (1 - maf)
-	freqAA = (1-maf)**2
+	mafHB = float(maf[0])
+	freqBB = mafHB**2
+	freqAB = 2 * mafHB * (1 - mafHB)
+	freqAA = (1-mafHB)**2
 	weigths_theo = np.asarray([freqAA,freqAB,freqBB])
-	gm = mixture.GaussianMixture(n_components=3, covariance_type="tied",weights_init=weigths_theo,means_init=means,precisions_init=precisions,random_state=0, max_iter=200, verbose=0, warm_start=False).fit(datas)
+	gm = mixture.GaussianMixture(n_components=3, covariance_type="tied",weights_init=weigths_theo,means_init=means_initialisation,random_state=0, max_iter=200, verbose=0, warm_start=False).fit(datas)
 	
 	resultatsGM = gm.predict(datas)
 	probasGM = gm.predict_proba(datas)
@@ -246,8 +270,9 @@ def GaussianMixture(df,wgts,means,genotypes,name_snp,index_ADN_103,maf,priors,pv
 	##### OUTPUT RESULTS #####
 	score = scoreHomoAfterGM(resultatsGM,index_ADN_103)
 	effectifs_after_GM = effectifs(resultatsGM)
+	MAF_obs = getMAFobs(effectifs_after_GM)
 	HB = hardyWeinberg(len(df),effectifs_after_GM,maf)
-	plot_results(datas,resultatsGM,means_res, cov_res,name_snp,index_ADN_103,score,maf,HB)
+	plot_results(datas,resultatsGM,means_res, cov_res,name_snp,index_ADN_103,score,maf,HB,MAF_obs)
 	#probas = writeProbasConfidence(df,"/home/e062531t/Bureau/Sidwell/res/probas_confidence_genotypes.txt", probasGM)
 	#writeCov("/home/e062531t/Bureau/Sidwell/res/cov_gm.txt",cov_res) 
 	#writeMeans("/home/e062531t/Bureau/Sidwell/res/means_gm.txt",means_res, genotypes)
@@ -310,24 +335,14 @@ def plotByPlate(datas,wgts,means,genotypes,name_snp): # plot results GM by plate
 	#plt.show()
 
 ################## HARDY WEINBERG #################
-def fequenceAlleles(df,wgts): # calcul frequence allelic from an all dataframe with all paltes 
-	freq_AA = wgts[0]
-	freq_AB = wgts[1]
-	freq_BB = wgts[2]
-	freq_A = freq_AA + ((1/2) * freq_AB)
-	freq_B = freq_BB + ((1/2) * freq_AB)
-	return freq_A, freq_B
 def hardyWeinberg(N,effectifs,maf): # return a chi2 
-	maf = float(maf)
+	maf = float(maf[0])
 	theo_eff_BB = N * (maf**2)
 	theo_eff_AB = 2 * N * maf * (1 - maf)
 	theo_eff_AA = N * ((1-maf)**2)
 	theo_effectifs = [theo_eff_AA, theo_eff_AB, theo_eff_BB]
 	pval = (scipy.stats.chisquare(effectifs, f_exp=theo_effectifs)[1])
 	return pval
-def desequilibreLiaison(df,wgts): # INCOMPLETE -> return DL but not sure of what it is 
-	freq_A, freq_B = fequenceAlleles(df,wgts)
-	DL = 0
 
 ################ SIMULATIONS ################
 def generateMultivarie(freq_allelique,n,size,df): # generate multivarie for simulations 
@@ -367,7 +382,7 @@ def generateMultivarie(freq_allelique,n,size,df): # generate multivarie for simu
 ############## REDEFINE PRIORS ##############
 def redefinePriors(df_snp,maf): # define priors from maf to re genotype after 
 	N = len(df_snp)
-
+	maf = maf[0]
 	nbBB = N * (maf**2)
 	nbAB = 2 * N * maf * (1 - maf)
 	nbAA = N * ((1-maf)**2)
@@ -375,22 +390,20 @@ def redefinePriors(df_snp,maf): # define priors from maf to re genotype after
 	nbAB = int(nbAB)
 	nbBB = int(nbBB)
 
-	df_snp = df_snp.sort_values(['CorrectedLogRatio'])
+	df_snp = df_snp.sort_values(['CorrectedLogRatio'],ascending=False)
 
-	new_priorBB_log = df_snp.iloc[int((0+nbBB)/2)]['CorrectedLogRatio']
-	priorBB_strength = df_snp.iloc[int((0+nbBB)/2)]['CorrectedStrength']
+	new_priorAA_log = df_snp.iloc[int((0+nbAA)/2)]['CorrectedLogRatio']
+	priorAA_strength = df_snp.iloc[int((0+nbAA)/2)]['CorrectedStrength']
 
 	a = int(nbAB/2)
-	new_priorAB_log = df_snp.iloc[nbBB+a]['CorrectedLogRatio']
-	priorAB_strength = df_snp.iloc[nbBB+a]['CorrectedStrength']
+	new_priorAB_log = df_snp.iloc[nbAA+a]['CorrectedLogRatio']
+	priorAB_strength = df_snp.iloc[nbAA+a]['CorrectedStrength']
 
-	b = int(nbAA/2)
-	new_priorAA_log = df_snp.iloc[nbBB+nbAB+b]['CorrectedLogRatio']
-	priorAA_strength = df_snp.iloc[nbBB+nbAB+b]['CorrectedStrength']
+	b = int(nbBB/2)
+	new_priorBB_log = df_snp.iloc[nbAA+nbAB+b]['CorrectedLogRatio']
+	priorBB_strength = df_snp.iloc[nbAA+nbAB+b]['CorrectedStrength']
 
 	priors = [[new_priorAA_log,priorAA_strength],[new_priorAB_log,priorAB_strength],[new_priorBB_log,priorBB_strength]]
-	#priors = [new_priorAA_log,new_priorAB_log,new_priorBB_log]
-	#print(priors)
 	priors = np.asarray(priors)
 
 	return priors
@@ -398,7 +411,7 @@ def redefinePriors(df_snp,maf): # define priors from maf to re genotype after
 
 ################## MAIN ################
 def main():
-	args = docopt.docopt(__doc__)
+	
 	genotypes = ["AA","AB","BB"]
 	AllDatas = importDatas(args['--callingRAC'])
 	AllDatas = AllDatas.sort_values(['snp','Sample'], ascending=[True,False]) #sort by snp to keep indices to plot after
@@ -408,24 +421,28 @@ def main():
 	#listSNP = ['rs7074353']
 
 	for snp in listSNP:
-		
 		df_snp = degPlatesFromSNP(AllDatas,snp)
-		df_corrected = correct_logRatio_centrality_etendue(correct_strength_median(df_snp))
-		wgtsGenotypes,meansGenotypes,countGenotypes,pvar = getParamsFromDatas(df_corrected,genotypes)
-
 		try:
 			maf = getMAF("all_maf_4_GM.txt",snp)
 		except FileNotFoundError:
 			print("Run getMAF.py first to get MAF from annotation file")
 			break	
+		if args['--correction'] == 'True':
+			df_to_GM = correct_logRatio_centrality_etendue(correct_strength_median(df_snp))
+			new_priors = redefinePriors(df_to_GM,maf)
+		else:
+			df_to_GM = df_snp
+			new_priors = 0	
+
+		wgtsGenotypes,meansGenotypes,countGenotypes,pvar = getParamsFromDatas(df_to_GM,genotypes)
 
 		print(snp)
-		new_priors = redefinePriors(df_corrected,maf)
 		
-		df_corrected = df_corrected.reset_index()
-		listADN = getADN103(df_corrected)
-		
-		GaussianMixture(df_corrected,wgtsGenotypes,meansGenotypes,genotypes,snp,listADN,maf,new_priors,pvar)
+		df_to_GM = df_to_GM.reset_index()
+		listADN = getADN103(df_to_GM)
+
+		GaussianMixture(df_to_GM,wgtsGenotypes,meansGenotypes,genotypes,snp,listADN,maf,new_priors,pvar)
+
 
 		#plotByPlate(df_snp,wgtsGenotypes,meansGenotypes,genotypes,snp)
 	
@@ -435,6 +452,7 @@ def main():
 ###### correction : modify GaussianMixture, GetParamsFromDatas, redefinePriors, main ###########
 
 if __name__ == '__main__' :
+	args = docopt.docopt(__doc__, version=__version__)
 	main()
 
 	
