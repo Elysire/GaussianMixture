f6aa6d283605aea6463b37663d41526cfefa06e7
Elysire <sidwell@rigade.com>
2017-01-31 16:43:18 +0100

    Get float MAF

--- getMAF.py
+++ getMAF.py
@@ -25,9 +25,9 @@ except ImportError:
     exit()
 import pandas as pd # pandas v0.19.1
 from itertools import chain
-pd.set_option('chained',None) #pour eviter le warning sur les copies de dataframe
+pd.set_option('chained',None) # pour eviter le warning sur les copies de dataframe
 
-def import_list_snp(listSNP):
+def import_list_snp(listSNP): # list of snp names
     list_snp = []
     with open(listSNP,'r') as f:
         for snp in f.readlines():
@@ -37,19 +37,25 @@ def import_list_snp(listSNP):
 
 def getMAF_from_annot_file34(filepath,name_snp): # get MAF for each snp 
     df = pd.read_csv(filepath,sep=',',header=19,low_memory=False)
-    if df[df['dbSNP RS ID']==name_snp]['Allele Frequencies'].any():
-        lineFreq = df[df['dbSNP RS ID']==name_snp]['Allele Frequencies']
-        if lineFreq.item() != "---":
+    if df[df['dbSNP RS ID']==name_snp]['Allele Frequencies'].any(): # if snp is this annot file
+        lineFreq = df[df['dbSNP RS ID']==name_snp]['Allele Frequencies'] # get the line with the snp at the allele frequencies column
+        if lineFreq.item() != "---": # if the frequence is not empty
             lineFreq = lineFreq.str.split("//")
             freq_A = lineFreq.item()[0]
+            freq_A = float(freq_A)
             freq_B = lineFreq.item()[1]
+            freq_B = float(freq_B)
+            print("A:",freq_A, "B:",freq_B)
             if freq_B < freq_A :
+                print("B smaller than A")
                 maf = [freq_B,"B"]
             if freq_A < freq_B:
+                print("A smaller than B")
                 maf = [freq_A,"A"]
+            print("SO maf = ",maf)
             return maf
         else:
-            maf=lineFreq.item()
+            maf=lineFreq.item() # the frequence is empty
     else :
         maf = "---"
     return maf
@@ -61,11 +67,17 @@ def getMAF_from_annot_file32(filepath,name_snp):
         lineFreq = lineFreq.str.split("//")
         freq_A = lineFreq.item()[3]
         freq_A = freq_A.replace("/ ","")
+        freq_A = float(freq_A)
         freq_B = lineFreq.item()[4]
+        freq_B = float(freq_B)
+        print("A:",freq_A, "B:",freq_B)
         if freq_B < freq_A :
+            print("B smaller than A")
             maf = [freq_B,"B"]
         if freq_A < freq_B:
+            print("A smaller than B")
             maf = [freq_A,"A"]
+        print("SO maf =",maf)
     return maf
 
     
@@ -73,17 +85,20 @@ def main():
     args = docopt.docopt(__doc__)
     tablo_all_maf=[]
     listSNP = import_list_snp(args['--snpList'])
-    #listSNP=['rs34440822']
+    #listSNP=['rs2487622']
     for snp in listSNP:
         maf = getMAF_from_annot_file34(args['--annotFile2'],snp)
-        if maf == "---":
+        if maf != "---":
+            print("ANNOT34")
+        if maf == "---": # if maf is not in annot34, it must be in annot32
+            print("ANNOT32")
             maf = getMAF_from_annot_file32(args['--annotFile1'],snp)
         couple_snp_maf_final = [snp,maf[0],maf[1]]
         print(couple_snp_maf_final)
         tablo_all_maf.append(couple_snp_maf_final)
     all_maf = pd.DataFrame(tablo_all_maf,columns=['snp','maf','allele'])
     print(all_maf)
-    all_maf.to_csv(path_or_buf="all_maf_4_GM.txt")
+    all_maf.to_csv(path_or_buf="list_mafs.txt") # return maf in a file to read it from an other script
     return all_maf
     
 
